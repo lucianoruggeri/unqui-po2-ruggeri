d21daa10dd2ffe4d5cf8f9340f72f3d943bafc7d
Luciano <Luciano@192.168.0.57>
2022-04-27 12:59:50 -0300

    TP4 ej2

--- src/ar/unq/po2/tp4/ProductoPrimeraNecesidad.java
+++ src/ar/unq/po2/tp4/ProductoPrimeraNecesidad.java
@@ -1,21 +1,19 @@
 package ar.unq.po2.tp4;
 
 public class ProductoPrimeraNecesidad extends Producto {
+	
+	private double descuento;
 
-	public ProductoPrimeraNecesidad(String nombre, double precio, boolean esPrecioCuidado) {
+	public ProductoPrimeraNecesidad(String nombre, double precio, boolean esPrecioCuidado, double descuento) {
 		super(nombre, precio, esPrecioCuidado);
-		
-	}
-
-	public ProductoPrimeraNecesidad(String nombre, double precio) {
-		super(nombre, precio);
+		this.descuento = descuento;
 		
 	}
 	
 	@Override
 	public double getPrecio() {
 		
-		return this.precio * 0.9;
+		return this.precio * (1 - this.descuento/100);
 	}
 	
 	
